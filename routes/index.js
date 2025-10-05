942fa40eae19b12ba50e7b0bff8f4ef63d1495fa
Lucas-Leal-Oliveira05 <lucas.l.oliveira0509@gmail.com>
2025-10-05 15:32:27 -0300

    PGFA-27 Criei página de login e página de cadastro, além de algumas alterações na página home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-
+//rota home
 router.get('/', (req, res) => {
   res.render('home', { title: 'Game Zone' });
+});
+
+//rota para pagina de login
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+//rota para pagina de cadastro
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'cadastro' });
 });
 
 module.exports = router;
